6e4fa9d47a662a052c16dee44f0b75bfc0bae4e0
leonnik <lencruseckij@gmail.com>
2024-10-13 18:54:48 +0300

    Update Generate

--- Assets/Scripts/MapGenerator/Biome/BiomeGenerator.cs
+++ Assets/Scripts/MapGenerator/Biome/BiomeGenerator.cs
@@ -17,7 +17,7 @@ public class BiomeGenerator : MonoBehaviour
     {
         MapData mapData = _mapSaver.LoadMap();
 
-            GenerateBiomes();
+        GenerateBiomes();
     }
 
     public void GenerateBiomes()
@@ -42,9 +42,8 @@ public class BiomeGenerator : MonoBehaviour
             }
             else
             {
-                // Новый биом размещается рядом с последним добавленным биомом
-                Rect lastBiomeRect = biomeRects[biomeRects.Count - 1];
-                position = FindBestPosition(lastBiomeRect, sizeX, sizeY, biomeRects);
+                // Новый биом размещается рядом с существующими биомами
+                position = FindBestPosition(sizeX, sizeY, biomeRects);
             }
 
             Rect biomeRect = new Rect(position.x, position.z, sizeX, sizeY);
@@ -138,6 +137,9 @@ public class BiomeGenerator : MonoBehaviour
             }
         }
 
+        // Заполнение пустых мест между биомами чанками
+        FillEmptySpacesWithChunks(biomeRects);
+
         MapData mapData = new MapData
         {
             Biomes = biomeDataList
@@ -146,46 +148,81 @@ public class BiomeGenerator : MonoBehaviour
         _mapSaver.SaveMap(mapData);
     }
 
-    private Vector3 FindBestPosition(Rect lastBiomeRect, int sizeX, int sizeY, List<Rect> biomeRects)
+    private Vector3 FindBestPosition(int sizeX, int sizeY, List<Rect> biomeRects)
     {
         Vector3[] directions = new Vector3[]
         {
-        new Vector3(lastBiomeRect.xMax, 0, lastBiomeRect.y), // Вправо
-        new Vector3(lastBiomeRect.x, 0, lastBiomeRect.yMax), // Вверх
-        new Vector3(lastBiomeRect.xMin - sizeX, 0, lastBiomeRect.y), // Влево
-        new Vector3(lastBiomeRect.x, 0, lastBiomeRect.yMin - sizeY), // Вниз
+            new Vector3(1, 0, 0), // Вправо
+            new Vector3(0, 0, 1), // Вверх
+            new Vector3(-1, 0, 0), // Влево
+            new Vector3(0, 0, -1), // Вниз
         };
 
         // Перемешиваем направления для случайного выбора
         System.Random random = new System.Random();
         directions = directions.OrderBy(a => random.Next()).ToArray();
 
-        foreach (var direction in directions)
+        foreach (var rect in biomeRects)
         {
-            Rect newBiomeRect = new Rect(direction.x, direction.z, sizeX, sizeY);
-            bool overlaps = false;
-
-            foreach (var rect in biomeRects)
+            foreach (var direction in directions)
             {
-                if (newBiomeRect.Overlaps(rect))
+                Vector3 newPosition = new Vector3(rect.x + direction.x * sizeX, 0, rect.y + direction.z * sizeY);
+                Rect newBiomeRect = new Rect(newPosition.x, newPosition.z, sizeX, sizeY);
+                bool overlaps = false;
+
+                foreach (var otherRect in biomeRects)
                 {
-                    overlaps = true;
-                    break;
+                    if (newBiomeRect.Overlaps(otherRect))
+                    {
+                        overlaps = true;
+                        break;
+                    }
                 }
-            }
 
-            if (!overlaps)
-            {
-                return direction;
+                if (!overlaps)
+                {
+                    return newPosition;
+                }
             }
         }
 
         // Если не найдено подходящее место, возвращаем начальную позицию
-        return new Vector3(lastBiomeRect.xMax, 0, lastBiomeRect.y);
+        return new Vector3(biomeRects[0].xMax, 0, biomeRects[0].y);
     }
 
+    private void FillEmptySpacesWithChunks(List<Rect> biomeRects)
+    {
+        // Определяем границы всех биомов
+        float minX = biomeRects.Min(r => r.x);
+        float maxX = biomeRects.Max(r => r.xMax);
+        float minY = biomeRects.Min(r => r.y);
+        float maxY = biomeRects.Max(r => r.yMax);
 
+        // Перебираем все клетки в пределах границ
+        for (float x = minX; x < maxX; x++)
+        {
+            for (float y = minY; y < maxY; y++)
+            {
+                bool isFilled = false;
 
+                foreach (var rect in biomeRects)
+                {
+                    if (rect.Contains(new Vector2(x, y)))
+                    {
+                        isFilled = true;
+                        break;
+                    }
+                }
+
+                if (!isFilled)
+                {
+                    // Создаем чанк в пустой клетке с настройками соседнего биома
+                    Vector3 position = new Vector3(x, 0, y);
+                    _biomeFactory.Create(_biomeSettings[0], position);
+                }
+            }
+        }
+    }
 
 
     private void LoadBiomes(MapData mapData)
